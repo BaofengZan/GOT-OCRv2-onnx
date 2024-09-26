d15f113e3c9c907d7600d2e1dcc87f99814d53f8
BaofengZan <zanbf1123@163.com>
2024-09-26 16:09:18 +0800

    uodate 使用onnxruntime-gpu

--- OnnxLLM/onnxllm/models/GOT/llm_got.py
+++ OnnxLLM/onnxllm/models/GOT/llm_got.py
@@ -50,8 +50,8 @@ class GotForCausalLM(QwenForCausalLM):
                 pbar.update(1)
 
     def get_attention_mask(self):
-        #if self.token_len:
-        #    return torch.zeros([1, 1, 1, self.seq_len], dtype=torch.float32)
+        if self.token_len:
+            return torch.zeros([1, self.seq_len], dtype=torch.float32)
         # 上三角矩阵
         causal_mask = torch.full(( self.seq_len, self.seq_len), fill_value=torch.finfo(torch.float32).min, dtype=torch.float32)
         
@@ -60,7 +60,7 @@ class GotForCausalLM(QwenForCausalLM):
         #return (1 - torch.tril(torch.ones([1, 1, self.seq_len, self.seq_len]))) * torch.finfo(torch.float32).min
     
     # forward 推理主流程
-    def forward(self, input_ids, attention_mask, position_ids, image):
+    def forward(self, input_ids, attention_mask, position_ids, past_key_caches, past_value_caches, image):
 
         # 1 先计算visual
         if self.first: # input_ids.shape[0] != 1:
@@ -84,30 +84,32 @@ class GotForCausalLM(QwenForCausalLM):
             self.image_start_token_pos = image_start_token_pos
         else:
             cur_input_embeds = self.embed(input_ids.astype(np.int64))
-            num_patches = self.image_feature.shape[1]
-            cur_input_embeds = np.hstack(
-                (
-                    cur_input_embeds[:,:self.image_start_token_pos+1, ...], 
-                    self.image_feature, 
-                    cur_input_embeds[:, self.image_start_token_pos + num_patches + 1:, ...]
-                )
-            )
+            # num_patches = self.image_feature.shape[1]
+            # cur_input_embeds = np.hstack(
+            #     (
+            #         cur_input_embeds[:,:self.image_start_token_pos+1, ...], 
+            #         self.image_feature, 
+            #         cur_input_embeds[:, self.image_start_token_pos + num_patches + 1:, ...]
+            #     )
+            # )
             #cur_input_embeds = np.transpose(cur_input_embeds, [1,0,2])
         # -------
-        presents = []
+        presents_key = []
+        presents_value = []
         #hidden_states = np.transpose(cur_input_embeds, [1, 0, 2]) # 【286，1，1024】--> [1,286,1024]
         hidden_states = cur_input_embeds  # [1,286,1024]
         for i in range(self.block_nums):
             #hidden_states = np.transpose(cur_input_embeds, [1, 0, 2])
-            hidden_states = self.blocks[i](hidden_states, attention_mask.astype(np.float32), position_ids)
-            #presents.append(kv)
+            hidden_states, key_states, value_states = self.blocks[i](hidden_states, attention_mask.astype(np.float32), position_ids,past_key_caches[i], past_value_caches[i])
+            presents_key.append(key_states)
+            presents_value.append(value_states)
         #hidden_states = np.transpose(cur_input_embeds, [1, 0, 2])
         hidden_states = self.norm(hidden_states)
         logits = self.lm(hidden_states[0])  # lmhead
         logits = torch.argmax(torch.from_numpy(logits[-1, :]), dim=-1)
         self.seq_len += 1
         self.token_len += 1
-        return logits
+        return logits,presents_key,presents_value
     # generate函数  调用推理函数
     def generate(self, **kwargs):
         input_ids = np.array(kwargs['input_ids'])
@@ -118,12 +120,14 @@ class GotForCausalLM(QwenForCausalLM):
         self.token_len = 0
         self.first=True
         #past_key_values = [np.zeros(self.past_kv_shape, dtype=np.float32) for i in range(self.block_nums)]
+        past_key_caches = [np.zeros((1,16,0,64), dtype=np.float32) for i in range(self.block_nums)]
+        past_value_caches = [np.zeros((1,16,0,64), dtype=np.float32) for i in range(self.block_nums)]
         token_id = np.copy(input_ids)
         output_ids = input_ids
         while self.token_len < max_new_tokens:
             attention_mask = self.get_attention_mask()[None][None]
             position_ids = self.get_position_ids()
-            token_id = self.forward(output_ids, attention_mask.numpy(), position_ids,imgs)
+            token_id, past_key_caches, past_value_caches = self.forward(token_id, attention_mask.numpy(), position_ids, past_key_caches, past_value_caches,  imgs)
             token_id = np.array([token_id.item()])
             output_ids = np.concatenate([output_ids, token_id])
             self.first=False
